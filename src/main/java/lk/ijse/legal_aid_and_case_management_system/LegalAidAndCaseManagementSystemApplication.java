b6e3e2cd31e6fe7bc94ab60a79909b538f7f1a29
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-03-06 19:22:43 +0530

    create spring boot project

---
+++ src/main/java/lk/ijse/legal_aid_and_case_management_system/LegalAidAndCaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.legal_aid_and_case_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalAidAndCaseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegalAidAndCaseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/legal_aid_and_case_management_system/LegalAidAndCaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.legal_aid_and_case_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalAidAndCaseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
