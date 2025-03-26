df5b869382f894a101cd10ee1efc3acdd196a004
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-03-26 20:57:36 +0530

    .

--- Legal_Aid_And_Case_Management_FrontEnd/js/clientDashboard.js
+++ Legal_Aid_And_Case_Management_FrontEnd/js/clientDashboard.js
@@ -45,12 +45,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // District options by province
   const districtsByProvince = {
+
     "Western": ["Colombo", "Gampaha", "Kalutara"],
     "Central": ["Kandy", "Matale", "Nuwara Eliya"],
     "Southern": ["Galle", "Matara", "Hambantota"],
     "Northern": ["Jaffna", "Kilinochchi", "Mannar", "Mullaitivu", "Vavuniya"],
-    "Eastern": ["Batticaloa", "Ampara", "Trincomalee"]
-    // Add more provinces and districts as needed
+    "Eastern": ["Batticaloa", "Ampara", "Trincomalee"],
+    "North Western": ["Kurunegala", "Puttalam"],
+    "North Central": ["Anuradhapura", "Polonnaruwa"],
+    "Uva": ["Badulla", "Monaragala"],
+    "Sabaragamuwa": ["Ratnapura", "Kegalle"]
+
+
+  // Add more provinces and districts as needed
   };
 
   const provinceSelect = document.getElementById("provinceSelect");
